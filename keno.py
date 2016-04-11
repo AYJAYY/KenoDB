ff116df148a076374831f1fc58805d2c61e2bd1b
AYJAYY <ajbbb@mail.com>
2016-04-11 13:46:55 -0400

    Updates

--- keno.py
+++ keno.py
@@ -14,9 +14,9 @@ def write_file(file_name, write_mode, file_text):
 
 
 #get the keno json file
-f = urllib2.urlopen("http://www.masslottery.com/data/json/search/dailygames/todays/15.json")
+ma_keno_json = urllib2.urlopen("http://www.masslottery.com/data/json/search/dailygames/todays/15.json")
 #read from the json file
-json_string = f.read()
+json_string = ma_keno_json.read()
 #parse the json file so we can work with it
 parsed_json = json.loads(json_string)
 #get the min and max game and subtract them
@@ -24,6 +24,11 @@ parsed_json = json.loads(json_string)
 min_game = int(parsed_json['min'])
 max_game = int(parsed_json['max'])
 games = max_game - min_game
+#log the min and max game for the day
+min_max = str(min_game) + str(max_game)
+sys_log = "KenoFiles/SYSLOG.csv"
+write_file(sys_log,"a+",min_max)
+
 
 #program loop
 while games > 0:
@@ -32,7 +37,7 @@ while games > 0:
         orgOrder = parsed_json['draws'][games]['winning_num_org']
         sortedOrder = parsed_json['draws'][games]['winning_num']
         multiplier = parsed_json['draws'][games]['bonus']
-        multi_value = parsed_json['draws'][games]['bonus_value']
+        multi_int = parsed_json['draws'][games]['bonus_value']
         draw = parsed_json['draws'][games]['draw_id']
         #split on dashes 19 times to split up the 20 numbers
         orgOrder_split = orgOrder.split('-', 19)
@@ -44,13 +49,15 @@ while games > 0:
         error_date = time.strftime("%Y-%m-%d-%I:%M %p")
         error_text =  str(e) + "," + "Number of Games Error" + "," + error_date + "\n"
         write_file(logfile, "a+", error_text)
+        sys_log = "KenoFiles/SYSLOG.csv"
+        write_file(sys_log,"a+",error_text)
         print "Too Few Games Played So Far. Currently at: " + str(games)
         games = games - 1
         continue
 
     #a way to string together the data using my "write file" function, this
     #also turns everything into a string format so I can concatenate them.
-    long_text = str(orgOrder_join + "," + orgOrder + "," + sortedOrder + "," + multiplier + "," + multi_value + "," + draw) + "\n"
+    long_text = str(orgOrder_join + "," + orgOrder + "," + sortedOrder + "," + multiplier + "," + multi_int + "," + draw) + "\n"
     #also put the numbers in a single row for alternate file
     single_row = str(orgOrder_column + "\n")
 
@@ -65,13 +72,18 @@ while games > 0:
         write_file("KenoFiles/kenodbfull.csv", "a+", long_text)
         #append to the single column file
         write_file("KenoFiles/kenodbfull-1column.csv", "a+", single_row)
-        #incase the user is running on demand, give success messages
+        #incase the user is running on demand, give success messages & log them
         print "Succesfully logged game #" + draw
+        vlog_string = "Succesfully logged game #" + "," + draw
+        sys_log = "KenoFiles/SYSLOG.csv"
+        write_file(sys_log,"a+",vlog_string)
     except Exception as eW:
         logfile_eW = "KenoFiles/LOG.csv"
         error_date_eW = time.strftime("%Y-%m-%d-%I:%M %p")
         error_text_eW =  str(eW) + "," + "File Write Error" + "," + error_date_eW + "\n"
         write_file(logfile_eW, "a+", error_text_eW)
+        sys_log = "KenoFiles/SYSLOG.csv"
+        write_file(sys_log,"a+",error_text_eW)
         print "An error has occured while writing to the file. Check the log in KenoFiles/LOG.csv"
         break
 
